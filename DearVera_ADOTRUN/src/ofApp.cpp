d4192ca0baea758aecbdb7dc0b5f359ca82df68c
Amit Runchal <amitrunchal@gmail.com>
2017-11-06 11:28:08 -0500

    re-added in GUI and fixed emotional state

--- DearVera_ADOTRUN/src/ofApp.cpp
+++ DearVera_ADOTRUN/src/ofApp.cpp
@@ -6,6 +6,8 @@ void ofApp::setup(){
     //it's in reproducing the art that I realize that I am about to get my ass kicked and that the work the
 
     ofBackground(250);
+    gui.setup();
+    gui.add(emotion.setup("emotion", 0.5,0,20));
 }
 
 //--------------------------------------------------------------
@@ -18,146 +20,42 @@ void ofApp::update(){
 //--------------------------------------------------------------
 void ofApp::draw(){
 
-    // i wrote this code because FUCK COORDINATE SYSTEMS and I was trying to understand them
-    // i am directionally dyslexic I think which makes the whole thing much harder
-    
-//    
-//    ofSetColor(10);
-//    ofPoint posA = ofPoint(10,40);
-//    ofPoint posB = ofPoint(60,10);
-//    ofPoint posC = ofPoint(30,40);
-//    ofPoint posD = ofPoint(80,10);
-//    ofPoint posE = ofPoint(50,40);
-//    ofPoint posF = ofPoint(100,10);
-//    ofPoint posG = ofPoint(70,40);
-//    
-//    ofDrawLine(posB, posA);
-//    ofDrawLine(posB, posC);
-//    ofDrawLine(posC, posD);
-//    ofDrawLine(posD, posE);
-//    ofDrawLine(posE, posF);
-    
-//    ofSetColor(0);
-//    ofPolyline myLine;
-//    for (int i = 0; i < 40; i++){
-//        if (i % 2 == 0){
-//            float x = 10 + i * 20;
-//            float y = 40 + (i*0);
-//            myLine.addVertex(x,y);
-//        } else {
-//            float x = 60 + i * 20;
-//            float y = 10 + (i*0);
-//            myLine.addVertex(x,y);
-//        }
-//    }
-//    myLine.draw();
-    
-
     
     
-    // a:
     
-//    ofSeedRandom(0);
-//    ofSetColor(0);
-//    ofPolyline myLine;
-//    for (int i = 0; i < 40; i++){
-//        if (i % 2 == 0){
-//            float x = 10 + i * 20 + ofRandom(-10,10);
-//            float y = 40 + (i*0);
-//            myLine.addVertex(x,y);
-//        } else {
-//            float x = 60 + i * 20 + ofRandom(-10,10);
-//            float y = 10 + (i*0);
-//            myLine.addVertex(x,y);
-//        }
-//    }
-//    myLine.draw();
-    
-    // b:
-    
-//    ofSeedRandom(0);
-//    ofSetColor(0);
-//    ofPolyline myLine;
-//    float x = 0;
-//    for (int i = 0; i < 40; i++){
-//        if (i % 2 == 0){
-//            float x2 = 10 + x;
-//            float y = 40 + (i*0);
-//            myLine.addVertex(x2,y);
-//        } else {
-//            float x2 = 60 + x;
-//            float y = 10 + (i*0);
-//            myLine.addVertex(x2,y);
-//        }
-//        
-//        x += ofRandom(mouseX,mouseY);
-//        
-//    }
-//    myLine.draw();
-    
-    
-    //ZACH: looks like he would use sin or ofNoise. Some of it is a linear progression (a distance function). Approach it as a linear problem. Linear is good
-    //with sin, for non-linear functions you want more noise, ofNoise is like sin in that you can pass in continuous points, but you're going to get more noisy results periodically
-
-    // ZACH: need to look at randomness, may want to add a counter to X, fixed positions that are offset from slightly to this
-    
-    
-//    ofSetColor(0);
-//    int vertSpacer = 30;
-//    
-//    for (int j = 0; j < 10; j++){
-//        ofPolyline myLine;
-//        for (int i = 0; i < 40; i++){
-//            if (i % 2 == 0){
-//                float x = 10 + i * 20 + ofRandom(-5,5);
-//                float y = 40 + (i*0) + j*vertSpacer;
-//                myLine.addVertex(x,y);
-//            } else {
-//                float x = 60 + i * 20 + ofRandom(-5,5);
-//                float y = 10 + (i*0) + j*vertSpacer;
-//                myLine.addVertex(x,y);
-//            }
-//        }
-//        myLine.draw();
-//    }
-    
-    
-//    ofSeedRandom(0);
+//    ofSeedRandom(0); // put this in to fix the emotional scale of response
     
     ofSetColor(0);
     float vertSpacer = 40;
     
-    //color chooser
-//    ofBackground(255);
-//    ofSetColor(ofMap(sin(ofGetElapsedTimef()) * 2, -1, 1, 30, 200),
-//               ofMap(sin(ofGetElapsedTimef()) * 3, -1, 1, 30, 200),
-//               ofMap(sin(ofGetElapsedTimef()) * 6, -1, 1, 30, 200));
-//
+    //line drawing
     
-//    //line drawing
-//    
-//    for (int h = 0; h < 40; h++){
-//        ofPolyline myLine;
-//        for (int i = 0; i < 80; i++){
-//            
-//            //what this is does is take a percentage of the range to map and create more chaos later in the function. Is this linear?
-////            float pct = ofMap(i, 0, 20, 0, 1); //the more the second value is increased, the more controlled this gets
-//            
-//            float pct = ofMap(i*(h*mouseX*0.01), 0, 1000, 0, 1); //the more the second value is increased, the less jagged the line gets
-//            
-//            if (i % 2 == 0){
-//                float x = 10 + i * 20 + ofRandom(-pct*10, pct*10);
-//                float y = 40 + (i*0) + h * vertSpacer;
-//                myLine.addVertex(x,y);
-//            } else {
-//                float x = 60 + i * 20 + ofRandom(-pct*10, pct*10);
-//                float y = 10 + (i*0) + h * vertSpacer;
-//                myLine.addVertex(x,y);
-//            }
-//        myLine.draw();
-//        }
-//    }
-////}
+    for (int h = 0; h < 40; h++){
+        ofPolyline myLine;
+        for (int i = 0; i < 80; i++){
+            
+            //what this is does is take a percentage of the range to map and create more chaos later in the function. Is this linear?
+//            float pct = ofMap(i, 0, 20, 0, 1); //the more the second value is increased, the more controlled this gets
+            
+            float pct = ofMap(i*(h*mouseX*0.01), 0, 1000, 0, 1); //the more the second value is increased, the less jagged the line gets
+            
+            if (i % 2 == 0){
+//                float x = 10 + i * 20 + ofRandom(-pct*10, pct*10);//                float x = 60 + i * 20 + ofRandom(-pct*10, pct*10); // this is before i made a param for emotion. It created sort of an effect where the program got more emotional the more they were writing to you
+                float x = 10 + i * 20 + ofRandom(-emotion*10, emotion*10); // one thing with this param being coded in this way is that it makes the emotional state of the piece constant — it is more a reflection of mental health than it is of an emotional response to a person which grows stronger over time
+                float y = 40 + (i*0) + h * vertSpacer;
+                myLine.addVertex(x,y);
+            } else {
+//                float x = 60 + i * 20 + ofRandom(-pct*10, pct*10); // this is before i made a param for emotion. It created sort of an effect where the program got more emotional the more they were writing to you
+                float x = 60 + i * 20 + ofRandom(-emotion*10, emotion*10); // one thing with this param is that it makes the emotional state of the piece constant — it is more a reflection of mental health than it is of an emotional response to a person which grows stronger over time
+                float y = 10 + (i*0) +  h * vertSpacer;
+                myLine.addVertex(x,y);
+            }
+        myLine.draw();
+        }
+        
+        gui.draw();
+    }
+//}
     
     
 }
--- DearVera_ADOTRUN/src/ofApp.h
+++ DearVera_ADOTRUN/src/ofApp.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxGui.h"
 
 class ofApp : public ofBaseApp{
 
@@ -22,5 +23,8 @@ class ofApp : public ofBaseApp{
 		void gotMessage(ofMessage msg);
     
     ofPolyline line;
+    ofPolyline slopeLine;
+    ofxFloatSlider emotion;
+    ofxPanel gui;
 		
 };
