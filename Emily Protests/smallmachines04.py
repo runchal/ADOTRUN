4351c1f498fb4a6b160b8eb927b8f4351ac2d200
Amit Runchal <amitrunchal@gmail.com>
2017-11-12 12:41:34 -0500

    edited emily tweets

--- Emily Protests/smallmachines04.py
+++ Emily Protests/smallmachines04.py
@@ -1,8 +1,9 @@
 # Emily Protest
 from random import choice
 first = ["Stop", "Oppose", "Support"]
-joiner = ["The", "--", "Some"]
+joiner = ["The", "--", "Some", "A"]
+punctuation
 second = ["Troops", "War", "Presidents"]
 
 for items in range(10):
-    print choice(first)
+    print (choice(first))
